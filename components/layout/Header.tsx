504b3cd11205a36d17cf68846b03bd27fe0ca2d2
Apple-cake <tongensai.kudasai@gmail.com>
2026-02-24 11:49:37 +0900

    ヘッダーにアイコン追加

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -2,17 +2,25 @@
  * サイト共通ヘッダー
  */
 
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="border-b bg-white">
-      <div className="mx-auto max-w-6xl px-4 py-4 md:py-6">
-        <h1 className="text-2xl md:text-3xl font-bold">
+    <header className="border-b border-zinc-200 bg-white">
+      <div className="mx-auto max-w-6xl px-3 py-4 md:px-7">
+        <h1 className="text-2xl font-bold">
           <Link
             href="/"
-            className="transition-opacity"
+            className="flex items-center transition-opacity"
           >
+            <Image
+              src="/icon.svg"
+              alt="Usagi Blog Logo"
+              width={30}
+              height={30}
+              className="md:w-10 md:h-10"
+            />
             Usagi Blog
           </Link>
         </h1>
