4597c649674f938bdd7a508f4e0de3cb3c39044d
Ryo Aiba <tingensai.kudasai@gmail.com>
2026-03-03 22:37:21 +0900

    ページネーションの表示上限を追加

--- components/article/ArticleArchive.tsx
+++ components/article/ArticleArchive.tsx
@@ -17,6 +17,48 @@ type Props = {
 };
 
 export default function ArticleArchive({ title, articles ,totalPages ,currentPage, basePath }: Props) {
+  function getPaginationPages(totalPages: number, currentPage: number) {
+    const pages: (number | "...")[] = [];
+
+    if (totalPages <= 7) {
+      return Array.from({ length: totalPages }, (_, i) => i + 1);
+    }
+
+    // 先頭付近
+    if (currentPage <= 3) {
+      for (let i = 1; i <= 5; i++) {
+        pages.push(i);
+      }
+      pages.push("...");
+      pages.push(totalPages - 1);
+      pages.push(totalPages);
+    }
+
+    // 末尾付近
+    else if (currentPage >= totalPages - 2) {
+      pages.push(1);
+      pages.push(2);
+      pages.push("...");
+      for (let i = totalPages - 4; i <= totalPages; i++) {
+        pages.push(i);
+      }
+    }
+
+    // 中央
+    else {
+      pages.push(1);
+      pages.push("...");
+
+      for (let i = currentPage - 2; i <= currentPage + 2; i++) {
+        pages.push(i);
+      }
+
+      pages.push("...");
+      pages.push(totalPages);
+    }
+
+    return pages;
+  }
   return (
     <>
         <section className="flex-1 min-w-0">
@@ -32,14 +74,21 @@ export default function ArticleArchive({ title, articles ,totalPages ,currentPag
             articles={articles}
             title={title}
         />
-        {totalPages && totalPages > 1 && (
-          <div className="flex gap-2 md:mx-4 mt-8 justify-center">
-            {Array.from({ length: totalPages }).map((_, i) => {
-              const page = i + 1;
-              const baseClass = "px-3 py-1 rounded-md border";
+        {totalPages && totalPages > 1 && currentPage && basePath && (
+          <div className="flex gap-1 md:mx-4 mt-8 justify-center flex-wrap">
+            {getPaginationPages(totalPages, currentPage).map((page, i) => {
+              const newPage = `${page}-${i}`;
+              if (page === "...") {
+                return (
+                  <span key={newPage} className="py-1 text-zinc-400">
+                    …
+                  </span>
+                );
+              }
+              const baseClass = "text-center px-1 w-8 mx-0.25 md:w-9 py-0.75 md:py-1 rounded-md border";
               return (
                 <Link
-                  key={page}
+                  key={newPage}
                   href={`${basePath}?page=${page}`}
                   className={`${baseClass} ${
                     page === currentPage
